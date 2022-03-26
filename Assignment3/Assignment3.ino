af1aa788a6ce05b22fe47b3012571477920bc34d
nl10001 <98903190+nl10001@users.noreply.github.com>
2022-03-26 21:53:06 +0000

    experimenting with task and different timing methods

--- Assignment3/Assignment3.ino
+++ Assignment3/Assignment3.ino
@@ -1,3 +1,25 @@
+//9700us maybe +50 not sure
+
+#include<stdio.h>
+
+#define LED1 15
+#define LED2 21
+#define NUM_TIMERS 7
+
+struct s_led {
+  byte          gpio;    // LED GPIO number
+  byte          state;  // LED state
+  unsigned      napms;  // Delay to use (ms)
+  TaskHandle_t  taskh;  // Task handle
+};
+
+static s_led leds[2] = {
+  { LED1, 0, 1000, 0 },
+  { LED2, 0, 500, 0 }
+};
+
+
+/*
 void foo(int counter) {
   printf("foo() called  ... count = %d\n", counter);
   vTaskDelay(1000);
@@ -6,17 +28,39 @@ void foo(int counter) {
   foo(counter+1);
   printf("\n");
 }
+*/
 
-void task(void *ignore) {
-  foo(1);  
+void vTask1(void * pvParameters) {
+  TickType_t xLastWakeTime;
+  const TickType_t xFrequency = 5000;
+  const TickType_t xDelay = 500;
+  // Initialise the xLastWakeTime variable with the current time.
+  xLastWakeTime = xTaskGetTickCount();
+    for( ;; ) { // Wait for the next cycle.
+      vTaskDelayUntil( &xLastWakeTime, xFrequency );
+      // Perform action here.
+      digitalWrite(LED2, HIGH);
+      vTaskDelay(xDelay);
+      digitalWrite(LED2, LOW);
+      Serial.println("done");
+    }
+
 }
 
+
+
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
-  xTaskCreate(&task, "task", 4096, NULL, 5, NULL);
+  pinMode(leds[0].gpio, OUTPUT);
+  pinMode(leds[1].gpio, OUTPUT);
+    
+  xTaskCreate(&vTask1, "Task 1", 1024, NULL, 5, NULL);
+  
+  //vTaskStartScheduler();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
